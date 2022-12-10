ebf884478672b24161013f164fb9d0323924e032
Hariharan Subramanian <hari@flowinsights.com>
2022-12-10 19:17:08 +0100

    Tracking tail movements with simple conditions

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["RopeBridge-AdventOfCodeDay9.csproj", "./"]
+RUN dotnet restore "RopeBridge-AdventOfCodeDay9.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "RopeBridge-AdventOfCodeDay9.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "RopeBridge-AdventOfCodeDay9.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "RopeBridge-AdventOfCodeDay9.dll"]
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("Analysing head movements...");
+var moves = File.ReadAllLines("head_movements.txt");
+
+var headPosition = (X: 0, Y: 0);
+var tailPosition = (X: 0, Y: 0);
+
+var headVisited = new HashSet<(int X, int Y)> {headPosition};
+var tailVisited = new HashSet<(int x, int y)> {tailPosition};
+
+// Puzzle 1
+foreach (var move in moves)
+{
+    var instruction = move.Split(" ");
+    var direction = instruction[0];
+    var steps = int.Parse(instruction[1]);
+
+    for (var i = 0; i < steps; i++)
+    {
+        // Move head
+        switch (direction)
+        {
+            case "L":
+                headPosition.X--;
+                break;
+            case "R":
+                headPosition.X++;
+                break;
+            case "U":
+                headPosition.Y++;
+                break;
+            case "D":
+                headPosition.Y--;
+                break;
+        }
+
+        headVisited.Add(headPosition);
+
+        // Move tail to follow head
+        if (Math.Abs(headPosition.X - tailPosition.X) >= 2 || Math.Abs(headPosition.Y - tailPosition.Y) >= 2)
+        {
+            if (headPosition.X == tailPosition.X) // if same row, move across column
+            {
+                if (headPosition.Y > tailPosition.Y) tailPosition.Y++;
+                else tailPosition.Y--;
+            }
+            else if (headPosition.Y == tailPosition.Y) // if same column, move across row
+            {
+                if (headPosition.X > tailPosition.X) tailPosition.X++;
+                else tailPosition.X--;
+            }
+            else // move diagonally
+            {
+                if (headPosition.X > tailPosition.X) tailPosition.X++;
+                else tailPosition.X--;
+
+                if (headPosition.Y > tailPosition.Y) tailPosition.Y++;
+                else tailPosition.Y--;
+            }
+        }
+
+        tailVisited.Add(tailPosition);
+    }
+}
+
+Console.WriteLine($"Tail visited {tailVisited.Count} unique positions.");
